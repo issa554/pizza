f9f548a67428bc908683ec03313359b7a76aa6d2
issa AbuAwada <67484019+issa554@users.noreply.github.com>
2024-03-31 22:19:36 +0200

    Update route.js

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,13 +1,11 @@
-import NextAuth from "next-auth"
-import CredentialsProvider from "next-auth/providers/credentials"
-import prisma from "../../../../../lib/db";
-import bcrypt from "bcrypt";
-import GoogleProvider from "next-auth/providers/google"
+const NextAuth = require("next-auth");
+const CredentialsProvider = require("next-auth/providers/credentials");
+const prisma = require("../../../../../lib/db");
+const bcrypt = require("bcrypt");
+const GoogleProvider = require("next-auth/providers/google");
 
-
-
-export const authOp = {
-  secret:process.env.SECRET,
+const authOp = {
+  secret: process.env.SECRET,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
@@ -15,28 +13,34 @@ export const authOp = {
     }),
     CredentialsProvider({
       name: 'Credentials',
-      id:"credentials",
+      id: "credentials",
       credentials: {
         username: { label: "Email", type: "email", placeholder: "example@example.com" },
         password: { label: "Password", type: "password" }
       },
       async authorize(credentials, req) {
-        const {email , password} = credentials
-        
+        const { email, password } = credentials;
+
         const user = await prisma.user.findUnique({
           where: {
             email
           }
         });
-        const passOk = user && bcrypt.compareSync( password,user.password)
-        if(passOk){
-          return user
+        const passOk = user && bcrypt.compareSync(password, user.password);
+        if (passOk) {
+          return user;
         }
-        return null
+        return null;
       }
     })
   ]
-}
-const handler = NextAuth(authOp)
+};
 
-export { handler as GET, handler as POST }
+const handler = async (req, res) => {
+  await NextAuth(req, res, authOp);
+};
+
+module.exports = { 
+  GET: handler, 
+  POST: handler 
+};
